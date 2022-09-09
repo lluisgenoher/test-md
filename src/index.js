4cfb0b6c1c5a3181f4f3833c6f14873946e12fd2
Lluis <lgenoherm@edu.tecnocampus.cat>
2022-09-09 11:20:06 +0200

    last commit

--- src/index.js
+++ src/index.js
@@ -3,66 +3,105 @@ import "./style.scss";
 var toolbar = document.querySelectorAll(".toolbar div");
 var allOption = document.querySelector(".allOption");
 var progressBar = document.querySelector(".progressBar div");
-var arrOptions = ["Opt 1 ", "Opt 2 ", "Opt 3"];
+var timeoutHandle;
+var arrOptions = { opt1: false, opt2: false, opt3: false };
+var textModified = "";
 
-allOption.addEventListener("click", function (event) {
-  allOption.classList.add("on");
+onClickAllOption();
+onClickOtherOptions();
 
-  progressBar.classList.add("progressBarFill");
+function onClickAllOption() {
+  allOption.addEventListener("click", function (event) {
+    resetAnimation();
+    resetArrOptions();
+    allOption.classList.add("on");
 
-  var timeoutHandle = setTimeout(function () {
-    document.getElementById("optionSelected").innerHTML = "All";
-    progressBar.classList.remove("progressBarFill");
-  }, 3000);
+    addProgressBar();
+    unmarkOthersOptions();
 
-  for (let i = 1; i < toolbar.length; i++) {
-    toolbar[i].classList.remove("on");
-  }
-});
-
-for (let i = 1; i < toolbar.length; i++) {
-  toolbar[i].addEventListener("click", function (event) {
-    if (!toolbar[i].classList.contains("on")) {
-      window.clearTimeout(timeoutHandle);
-
-      progressBar.classList.add("progressBarFill");
-      toolbar[i].classList.add("on");
-      unmarkAllOption();
-
-      var timeoutHandle = setTimeout(function () {
-        printText(i);
-        progressBar.classList.remove("progressBarFill");
-      }, 3000);
-    }
+    timeoutHandle = setTimeout(function () {
+      removeProgressBar();
+      changeText("All");
+    }, 3000);
   });
 }
 
+function onClickOtherOptions() {
+  for (let i = 1; i < toolbar.length; i++) {
+    toolbar[i].addEventListener("click", function (event) {
+      resetAnimation();
+
+      if (!toolbar[i].classList.contains("on")) {
+        addProgressBar();
+        toolbar[i].classList.add("on");
+        unmarkAllOption();
+        printText(i);
+        timeoutHandle = setTimeout(function () {
+          changeText(textModified);
+          removeProgressBar();
+        }, 3000);
+      }
+    });
+  }
+}
+
+function resetAnimation() {
+  clearTimeout(timeoutHandle);
+  progressBar.style.animation = "none";
+  progressBar.offsetHeight; /* trigger reflow */
+  progressBar.style.animation = null;
+}
+function resetArrOptions() {
+  arrOptions = { opt1: false, opt2: false, opt3: false };
+}
+function addProgressBar() {
+  progressBar.classList.add("progressBarFill");
+}
+function removeProgressBar() {
+  progressBar.classList.remove("progressBarFill");
+}
 function unmarkAllOption() {
   if (allOption.classList.contains("on")) {
     allOption.classList.remove("on");
-    document.getElementById("optionSelected").innerHTML = "";
   }
 }
-function printText(numOption) {
-  var textAdded;
+function unmarkOthersOptions() {
+  for (let i = 1; i < toolbar.length; i++) {
+    toolbar[i].classList.remove("on");
+  }
+}
 
+function printText(numOption) {
   var currentText = document.getElementById("optionSelected");
   switch (numOption) {
     case 1:
-      var newText = "Opt 1, " + currentText.innerHTML;
-      currentText.appendChild(document.createTextNode(newText));
+      arrOptions[0] = true;
+      if (arrOptions[1] == true || arrOptions[2] == true) {
+        textModified = "Opt 1, " + textModified;
+      } else textModified = "Opt 1";
       break;
     case 2:
-      currentText.appendChild(document.createTextNode("Opt 2, "));
-
+      arrOptions[1] = true;
+      if (arrOptions[0] === true) {
+        if (arrOptions[2] === true) {
+          textModified = "Opt1, Opt2, Opt 3";
+        } else textModified = ", Opt 2";
+      } else {
+        if (arrOptions[2] === true) {
+          textModified = "Opt 2, " + textModified;
+        } else textModified = "Opt 2";
+      }
       break;
+
     case 3:
-      currentText.appendChild(document.createTextNode("Opt 3"));
+      arrOptions[2] = true;
+      if (arrOptions[0] == true || arrOptions[1] == true) {
+        textModified = textModified + ", Opt 3";
+      } else textModified = "Opt 3";
       break;
   }
 }
-function addStr(str, index, stringToAdd) {
-  return (
-    str.substring(0, index) + stringToAdd + str.substring(index, str.length)
-  );
+
+function changeText(newText) {
+  document.getElementById("optionSelected").replaceChildren(newText);
 }
